14d81fccad7d6fedb8f3e13f004e6b4d16a368d2
Maria Heloisa <maria.henrique@dcx.ufpb.br>
2026-03-23 23:35:11 -0300

    Concertando erros

--- ProjetoPOOSistemaLojaGas2/src/test/java/br/ufpb/MH/CaixaTest.java
+++ ProjetoPOOSistemaLojaGas2/src/test/java/br/ufpb/MH/CaixaTest.java
@@ -2,7 +2,6 @@ package br.ufpb.MH;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CaixaTest {
